f0e934e191abc5fda9a69fe141b463755769208a
Praveena Manikonda <praveena@mangoyard.local>
2012-07-31 23:44:01 -0700

    Brand new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mangoyard::Application.config.secret_token = '93f5f62382400f78245c02fcb69e31f3f16dea62873489541125569ba0a0aba3df66e99759a1cab43482ff50343574aa8fcc70291b349237e764ad90ccb93456'
